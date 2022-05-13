e2400598c6da807ac700f5cf6f0257abbaff04bd
dselmy <dselmy@student.21-school.ru>
2022-05-13 18:54:55 +0300

    mlx error management

--- init_mlx.c
+++ init_mlx.c
@@ -6,22 +6,32 @@
 /*   By: dselmy <dselmy@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 23:18:29 by dselmy            #+#    #+#             */
-/*   Updated: 2022/05/13 16:07:37 by dselmy           ###   ########.fr       */
+/*   Updated: 2022/05/13 18:48:47 by dselmy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	put_error(char *path)
+{
+	ft_putstr_fd("MLX error\n", 2);
+	if (path)
+		perror(path);
+	else
+		perror("");
+	return (-1);
+}
+
 static int	get_tex_img(void *mlx, char *path, t_img *tex)
 {
 	tex->img = mlx_xpm_file_to_image(mlx, path, &(tex->width), &(tex->height));
 	if (!tex->img)
-		return (0);
+		return (put_error(path));
 	tex->addr = mlx_get_data_addr(tex->img, &(tex->bpp), \
 									&(tex->line_len), &(tex->en));
 	if (!tex->addr)
-		return (0);
-	return (1);
+		return (put_error(path));
+	return (0);
 }
 
 static int	get_texture(t_win *win, t_config *cnfg)
@@ -31,7 +41,7 @@ static int	get_texture(t_win *win, t_config *cnfg)
 	i = 0;
 	while (i < 4)
 	{
-		if (!get_tex_img(win->mlx, cnfg->tex_paths[i], &(win->textures[i])))
+		if (get_tex_img(win->mlx, cnfg->tex_paths[i], &(win->textures[i])) < 0)
 			return (-1);
 		i += 1;
 	}
@@ -42,20 +52,20 @@ int	start_win(t_win *win, t_config *cnfg)
 {
 	win->mlx = mlx_init();
 	if (!win->mlx)
-		return (-1);
+		return (put_error(NULL));
 	mlx_get_screen_size(win->mlx, &(win->x_win), &(win->y_win));
 	win->prop = (double)win->x_win / (double)win->y_win;
 	win->img = mlx_new_image(win->mlx, win->x_win, win->y_win);
 	if (!win->img)
-		return (-1);
+		return (put_error(NULL));
 	win->addr = mlx_get_data_addr(win->img, &(win->bpp), \
 								&(win->line_len), &(win->en));
 	if (!win->addr)
-		return (-1);
+		return (put_error(NULL));
 	if (get_texture(win, cnfg) < 0)
 		return (-1);
 	win->win = mlx_new_window(win->mlx, win->x_win, win->y_win, "cub3d");
 	if (!win->win)
-		return (-1);
+		return (put_error(NULL));
 	return (0);
 }
--- start_game.c
+++ start_game.c
@@ -6,7 +6,7 @@
 /*   By: dselmy <dselmy@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 15:07:58 by dselmy            #+#    #+#             */
-/*   Updated: 2022/05/13 15:55:27 by dselmy           ###   ########.fr       */
+/*   Updated: 2022/05/13 18:47:16 by dselmy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,7 @@ void	print_config(t_config *cnfg, char **map)
 int		cub(t_data *all)
 {
 	if (start_win(all->win, all->cnfg) < 0)
-	{
-		ft_putstr_fd("Error:\nmlx error.\n", 2);
 		stop_game(all);
-	} // error management
-	// put_map(all->win, all->cnfg, all->map);
 	all->plr_data->x_win = all->map_width;
 	all->plr_data->y_win = all->map_h;
 	put_screen(all);
